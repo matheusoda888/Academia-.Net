b9f6a41a3a4dcecef682b0cd68adbdf2486cd9ef
Matheus Fortes <58231046+matheusoda888@users.noreply.github.com>
2022-10-12 01:02:12 -0300

    Add files via upload

---
+++ Lista7Ex1/Lista7/Livro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista7
+{
+    internal class Livro
+    {
+        public string nome;
+        public string genero;
+        public int paginas;
+
+        public Livro(string nome, string genero, int paginas)
+        {
+            this.nome = nome;
+            this.genero = genero;
+            this.paginas = paginas;
+        }
+
+
+    }
+}
---
+++ Lista7Ex1/Lista7/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Lista7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int continuar = 1;
+            string nome;
+            string genero;
+            int paginas;
+            Livro livro;
+            List<Livro> livros = new List<Livro>();
+            while (continuar!=0)
+            {
+                Console.WriteLine("nome do livro: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("genero do livro: ");
+                genero = Console.ReadLine();
+                Console.WriteLine("numero de pginas: ");
+                paginas = int.Parse(Console.ReadLine());
+                livro = new Livro(nome, genero, paginas);
+                livros.Add(livro);
+                Console.WriteLine("digite zero para sair");
+                continuar=int.Parse(Console.ReadLine());
+                
+
+            }
+            foreach (var item in livros)
+            {
+                Console.WriteLine(item.nome);
+                Console.WriteLine(item.genero);
+                Console.WriteLine(item.paginas);
+            }
+
+
+        }
+    }
+}
