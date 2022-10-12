3924c1c47c88f222157fba28130c1c69762e8457
Matheus Fortes <58231046+matheusoda888@users.noreply.github.com>
2022-10-12 01:04:16 -0300

    Add files via upload

---
+++ Lista7Ex2/Lista7Ex2/Pessoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista7Ex2
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+        public string sexo;
+        public Pessoa(string nome, int idade, string sexo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.sexo = sexo;
+        }
+        public string getNome() { 
+            return nome;
+        }
+        public int getIdade()
+        {
+            return idade;
+        }
+        public string getSexo()
+        {
+            return sexo;
+        }
+
+    }
+}
---
+++ Lista7Ex2/Lista7Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Lista7Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int continuar = 1;
+            string nome;
+            string sexo;
+            int idade;
+            Pessoa pessoa;
+            List<Pessoa> pessoas = new List<Pessoa>();
+            while (continuar != 0)
+            {
+                Console.WriteLine("nome: ");
+                nome = Console.ReadLine();
+                Console.WriteLine("sexo: ");
+                sexo = Console.ReadLine();
+                Console.WriteLine("idade: ");
+                idade = int.Parse(Console.ReadLine());
+                pessoa = new Pessoa(nome,idade,sexo);
+                pessoas.Add(pessoa);
+                Console.WriteLine("digite zero para sair");
+                continuar = int.Parse(Console.ReadLine());
+
+
+            }
+            foreach (var item in pessoas)
+            {
+                Console.WriteLine(item.getNome());
+                Console.WriteLine(item.getIdade());
+                Console.WriteLine(item.getSexo());
+            }
+        }
+    }
+}
