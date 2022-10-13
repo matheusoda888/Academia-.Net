b34f65b4cb7b64361f3856664bde2a8103f7ab19
Matheus Fortes <58231046+matheusoda888@users.noreply.github.com>
2022-10-13 07:37:28 -0300

    Add files via upload

---
+++ Lista7Ex7/Lista7Ex7/Elevador.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista7Ex7
+{
+    internal class Elevador
+    {
+        public int andar=0;
+        public int andarMax;
+        public int pessoas=0;
+        public int pessoasMax;
+        public Elevador( int andarMax, int pessoasMax)
+        {
+            
+            this.andarMax = andarMax;
+            
+            this.pessoasMax = pessoasMax;
+        }
+        public void entra()
+        {
+            if (pessoas < pessoasMax) { 
+            this.pessoas++;
+            }
+        }
+        public void sai()
+        {
+            if (pessoas > 0)
+            {
+                this.pessoas--;
+            }
+        }
+        public void sobe()
+        {
+            if (andar < andarMax)
+            {
+                andar++;
+            }
+        }
+        public void desce()
+        {
+            if (andar > 0)
+            {
+                andar--;
+            }
+
+        }
+        public void imprimirInfo()
+        {
+            Console.WriteLine(andarMax + " andares");
+            Console.WriteLine(andar + " andar atualmente");
+            Console.WriteLine(pessoas + " pessoas atualmente");
+            Console.WriteLine(pessoasMax + " de capacidade máxima");
+        }
+    }
+}
---
+++ Lista7Ex7/Lista7Ex7/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Lista7Ex7
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {   Elevador elevador;
+            int andarMax;
+            int pessoasMax;
+            Console.WriteLine("Quantos andares tem o prédio?");
+            andarMax = int.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a capacidade maxima do elevador?");
+            pessoasMax = int.Parse(Console.ReadLine());
+            elevador = new Elevador(andarMax,pessoasMax);
+
+            int continuar = 1;
+            int op = 0;
+            while (continuar != 0)
+            {
+                Console.WriteLine("digite 1 para subir, 2 para descer, 3 para adicionar, 4 para retirar, 5 para imprimir");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        elevador.sobe();
+                        break;
+                    case 2:
+                        elevador.desce();
+                        break;
+                    case 3:
+                        elevador.entra();
+                        break;
+                    case 4:
+                        elevador.sai();
+                        break;
+                    case 5:
+                        elevador.imprimirInfo();
+                        break;
+                }
+            }
+
+        } 
+    }
+}
