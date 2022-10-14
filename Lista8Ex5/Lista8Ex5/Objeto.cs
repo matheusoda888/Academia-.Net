06d1038d249d83e53217b86e66a46c6c808afa06
Matheus Fortes <58231046+matheusoda888@users.noreply.github.com>
2022-10-14 13:49:27 -0300

    Add files via upload

---
+++ Lista8Ex5/Lista8Ex5/Objeto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista8Ex5
+{
+    internal class Objeto
+    {
+        int x, y;
+        string descricao;
+        public Objeto()
+        {
+            
+        }
+        public Objeto(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            this.descricao = "sem descricao";
+        }
+        public void setX(int valor)
+        {
+            x = valor;
+        }
+        public void setY(int valor)
+        {
+            y = valor;
+        }
+        public int getX() { return x; }
+        public int getY() { return y; }
+
+        public void exibirObjeto()
+        {
+            
+            Console.WriteLine("("+x + "," + y+")");
+        }
+    }
+}
---
+++ Lista8Ex5/Lista8Ex5/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Lista8Ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int x = 0,y=0;
+            List <Objeto> objetos = new List <Objeto> ();   
+            Objeto objeto;
+            objeto = new Objeto();
+            
+            Console.WriteLine("Digite quantos objetos deseja cadastrar: ");
+            n=int.Parse (Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int u = i + 1;
+                Console.WriteLine("digite o X do obj "+u);
+                x=int.Parse (Console.ReadLine());
+                
+                Console.WriteLine("digite o Y do obj " + u);
+                y = int.Parse(Console.ReadLine());
+                objeto = new Objeto(x, y);
+                objetos.Add(objeto);
+            }
+            int c = 1;
+            foreach(Objeto item in objetos)
+            {
+                
+                Console.WriteLine("item "+c);
+                item.exibirObjeto();
+                c++;
+            }
+        }
+    }
+}
