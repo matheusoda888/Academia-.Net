8645c6a5492ea1b4eefbb5da5a75ea0f5eee1a82
Matheus Fortes <58231046+matheusoda888@users.noreply.github.com>
2022-10-14 13:47:54 -0300

    Add files via upload

---
+++ Lista8Ex8/Lista8Ex8/Livro.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista8Ex8
+{
+    internal class Livro
+    {
+        string nome;
+        int paginas;
+        public Livro(string nome, int paginas)
+        {
+            this.nome = nome;
+            this.paginas = paginas;
+        }
+        public string getNome()
+        {
+            return nome;
+        }
+        public int getPaginas()
+        {
+            return paginas;
+        }
+        public void exibirLivro()
+        {
+            Console.WriteLine("Nome: "+nome);
+            Console.WriteLine("Paginas: "+paginas);
+        }
+        
+    }
+}
---
+++ Lista8Ex8/Lista8Ex8/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Lista8Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Livro livro;
+            string nome;
+            int paginas;
+            IDictionary<int,Livro> livros = new Dictionary<int,Livro>();
+            for (; ; )
+            {
+                int op = 0;
+                Console.WriteLine("Digite 1 para adicionar livros, 2 para buscar um livro e 3 para mostrar todos os livros");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        for (int i = 1; i != 0;)
+                        {
+                            Console.WriteLine("digite o nome do livro " + (livros.Count + 1));
+                            nome = Console.ReadLine();
+                            Console.WriteLine("digite o numero de paginas do livro " + (livros.Count + 1));
+                            paginas = int.Parse(Console.ReadLine());
+                            livro = new Livro(nome, paginas);
+                            livros.Add(livros.Count, livro);
+                            Console.WriteLine("digite zero para parar os cadastros ");
+                            i = int.Parse(Console.ReadLine());
+                        }
+                        break;
+                    case 2:
+                        int j = 0;
+                        string nomeBusca;
+                        Console.WriteLine("digite o nome do livro");
+                        nomeBusca = Console.ReadLine().ToLower();
+                        foreach (KeyValuePair<int, Livro> item in livros)
+                        {
+
+                            if (nomeBusca == item.Value.getNome().ToLower())
+                            {
+                                Console.WriteLine((item.Key + 1) + " é a posicao do livro buscado");
+                                j = 1;
+
+                            }
+                        }
+                        if (j == 0)
+                        {
+                            Console.WriteLine("Nenhum livro encontrado");
+                        }
+                        break;
+                    case 3:
+                        foreach(KeyValuePair<int,Livro> item in livros)
+                        {
+                            Console.WriteLine(item.Key);
+                            item.Value.exibirLivro();
+                            
+                        }
+                        break;
+
+
+
+                }
+                
+
+
+
+                
+
+            }
+
+
+
+        }
+    }
+}
