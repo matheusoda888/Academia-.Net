f01cc08838a226c9c673aa7377721fe4fe0bea1d
Matheus Fortes <58231046+matheusoda888@users.noreply.github.com>
2022-10-24 15:10:33 -0300

    Add files via upload

---
+++ Lista9Ex1/Lista9Ex1/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Lista9Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> frases = new List<string>();
+            string frase = "";
+
+            Util.popularArquivo(frases, "nomeArquivo.txt");
+            while (true)
+            {
+                
+                Console.WriteLine("digite uma frase ou sair");
+                frase = Console.ReadLine();
+                    if(frase.ToUpper() == "SAIR")
+                    {
+                    
+                        break;
+                    }
+
+
+                //adicionar a lista caso não exista ainda na mesma
+                if (frases.Contains(frase.ToUpper()))
+                {
+                    Console.WriteLine("Frase ja cadastrada");
+                }
+                else if(!frase.StartsWith(" ")&& !frase.EndsWith(" ")&&frase.Contains(" ")){
+                    frases.Add(frase.ToUpper());
+                    //Gravar frase no arquivo
+                    Util.gravar(frase, "nomeArquivo.txt");
+                }
+                else
+                {
+                    Console.WriteLine("somente nomes completos");
+                    
+                }
+
+                
+               
+            }
+            foreach(string item in frases)
+            {
+                Console.WriteLine(item);
+            }
+            //Util.gravar(frase,"nomeDoArquivo.txt");
+        }
+    }
+}
---
+++ Lista9Ex1/Lista9Ex1/Util.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista9Ex1
+{
+    internal class Util
+    {
+        public static void gravar(string frase, string nomeArquivo)
+        {
+            StreamWriter escritor = new StreamWriter(nomeArquivo, append: true);
+            escritor.WriteLine(frase.ToUpper());
+            escritor.Flush();
+            escritor.Close();
+        }
+        public static void popularArquivo(List<string> lista, string nomeArquivo)
+        {
+            try
+            {
+                StreamReader leitor = new StreamReader(nomeArquivo);
+                do
+                {
+                    lista.Add(leitor.ReadLine().ToUpper());
+                } while (!leitor.EndOfStream);
+
+                leitor.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
